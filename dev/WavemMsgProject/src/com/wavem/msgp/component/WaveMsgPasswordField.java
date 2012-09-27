b93a91498b7026aeffaa3946a91f2cc06f9d55b4
HG01362 <wody8674@hanmail.net>
2012-09-27 09:43:22 +0900

    임시 삭제

--- dev/WavemMsgProject/src/com/wavem/msgp/component/WaveMsgPasswordField.java
+++
@@ -1,9 +0,0 @@
-package com.wavem.msgp.component;
-
-import javax.swing.JPasswordField;
-
-public class WaveMsgPasswordField extends JPasswordField {
-
-	private static final long serialVersionUID = -8594427702295945496L;
-
-}
